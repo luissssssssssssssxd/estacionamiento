3a47d2a15843fa904cad006bb0fa7cd480be8b2d
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-05-28 22:25:42 -0400

    CORS y Express

---
+++ README.md
@@ -0,0 +1,6 @@
+#Best sistemas LAS
+ni siquiera yo se como funciona esto
+28/05/2022
+
+
+npm install
---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
+
+
+const {dbConnection} = require('./database/config');
+
+
+//Crear el servidor de express
+const app = express();
+
+
+//Configurar CORS
+app.use(cors());
+
+//Base de datos
+dbConnection();
+
+
+//Rutas
+app.get('/',(req,res)=>{
+    res.json({
+        ok:true,
+        msg:'Hola mundo'
+    })
+});
+
+app.listen(process.env.PORT,()=>{
+    console.log('App corriendo en puerto '+ process.env.PORT)
+})
